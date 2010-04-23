ae35ba8185f3593f540e4aabac3fb44bc09ed5b2
Bob <bob@electricgraffitti.com>
2010-04-23 14:31:10 -0600

    more foo

--- public/javascripts/pageLayouts.js
+++ public/javascripts/pageLayouts.js
@@ -1,10 +1,8 @@
 // var baseLayout = $('#panel_layout').layout({
 //   applyDefaultStyles:       true,
 //   west__applyDefaultStyles: false,
-//   west__paneSelector: "#main_west",
 // });
 // 
 // var innerLayout = $('#panel_layout > .ui-layout-center').layout({
 //   applyDefaultStyles: true,
-//   south__paneSelector: "#main_south"
 // });
