57df9934ae2b25e0ec09c063f0234b220393a265
Bob <bob@electricgraffitti.com>
2010-05-13 09:53:01 -0600

    more reports form

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -117,6 +117,16 @@ var adminAction = {
       			buttonImage: '../images/inner_pages/date_picker_cal.jpg',
       			buttonImageOnly: true
 		});
+		$('#schedule_start_date').datepicker({
+		  showOn: 'button',
+      			buttonImage: '../images/inner_pages/date_picker_cal_white.jpg',
+      			buttonImageOnly: true
+		});
+		$('#schedule_end_date').datepicker({
+		  showOn: 'button',
+      			buttonImage: '../images/inner_pages/date_picker_cal_white.jpg',
+      			buttonImageOnly: true
+		});
 	}
   
 };
