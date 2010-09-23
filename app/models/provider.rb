47f0522182e66699f23c5b9a9c64a7f6f81e4d41
Bob Hanson <bob@bobmac.hcinsight.net>
2010-09-23 12:06:44 -0600

    new modal

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -17,43 +17,42 @@
 #
 
 class Provider < ActiveRecord::Base
-  
+
   include Graphs
-  
+
   belongs_to :provider_group
   belongs_to :provider_speciality
   belongs_to :state
-  
+
   has_many :claims
   has_many :patients, :through => :claims
   has_one :provider_stat
-  
+
   has_many :conditions
   has_many :condition_stats, :through => :conditions
-  
+
   has_many :locations, :class_name => "Location"
   accepts_nested_attributes_for :locations, :allow_destroy => true
-  
+
   named_scope :small_list, lambda { |limit| {:limit => limit}}
   named_scope :top_billable, :order => "provider_score DESC"
-  
-  
+
+
   def full_name
     full_name = self.first_name + " " + self.last_name
     return full_name
   end
-  
+
   def score_color(s)
-    
+
     score = s.provider_score
-    
+
     score_color = case score
-      when 800..1000: "red"
-      when 450..799: "yellow"
-      else "green"
+    when 800..1000: "red"
+    when 450..799: "yellow"
+    else "green"
     end
     return score_color
   end
-  
-end
 
+end
