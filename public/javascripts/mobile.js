a6e8df7f7258dec0853c921a4b97fb2655ca91ac
Bob Hanson <bob@bobmac.hcinsight.net>
2010-09-29 12:52:50 -0600

    working on the mobile drag n drop

---
+++ public/javascripts/mobile.js
@@ -0,0 +1,8 @@
+var ipad = {
+  
+  dnd: function() {
+    $(".quick_launch_box").addClass('mobiledraganddropdrop');
+    $(".quick_launch_box").mobiledraganddrop({ targets: "#quick_launch_top" });
+  }
+  
+}
