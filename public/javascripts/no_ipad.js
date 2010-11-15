d5aff8d6b0a6e9b4ad5e15f8b403add5d69dca32
Bob Hanson <Bob@super17.local>
2010-11-14 20:17:48 -0800

    provider action drag

--- public/javascripts/no_ipad.js
+++ public/javascripts/no_ipad.js
@@ -32,6 +32,23 @@ var drag = {
       update: function() { drag.setDashboardLaunchOrder(ss,scn,sce); }
     });
   },
+	
+	providerActionDrag: function(a,b,c) {
+		// set the Selector
+    var ss = a;
+    // console.log(a);
+    // set cookie name
+    var scn = b;
+    // set cookie expiry time (days):
+    var sce = c;
+    
+    ss.sortable({
+      handle : '.widget_header',
+      opacity : 0.3,
+      cursor: "move",
+      update: function() { drag.setProviderActionOrder(ss,scn,sce); }
+    });
+	},
   
   setQuickLaunchOrder: function(ss,scn,sce) {
     $.cookie(scn, ss.sortable("toArray"), { expires: sce, path: "/" });
@@ -40,6 +57,10 @@ var drag = {
   setDashboardLaunchOrder: function(ss,scn,sce) {
     $.cookie(scn, ss.sortable("toArray"), { expires: sce, path: "/" });
   },
+
+	setProviderActionOrder: function(ss,scn,sce) {
+		$.cookie(scn, ss.sortable("toArray"), {expires: sce, path:"/"});
+	},
   
   loadDashboardLaunchOrder: function(ss,scn) {
     var list = ss;
@@ -156,10 +177,68 @@ var drag = {
     }
   },
   
-  tutorialDrag: function() {
+	loadProviderActionOrder: function(ss,scn) {
+    var list = ss;
+    
+    if (list == null) return
+
+    // fetch the cookie value (saved order)
+    var cookie = $.cookie(scn);
+    if (!cookie) {
+      return
+    };
+
+    // make array from saved order
+    var IDs = cookie.split(",");
+    
+    // fetch current order
+    var items = list.sortable("toArray");
+		console.log(items);
+		
+    // make array from current order
+    var rebuild = new Array();
+    
+    for ( var v=0, len=items.length; v<len; v++ ) {
+      rebuild[items[v]] = items[v];
+    }
+
+    for (var i = 0, n = IDs.length; i < n; i++) {
+      var itemID = IDs[i];
+      
+      if (itemID in rebuild) {
+        var item = rebuild[itemID];
+
+        
+        // select the item according to current order
+        var child = ss.children("#" + item);
+
+        // select the item according to the saved order
+        // if (widget.checkQLCookie(item)) {
+        //   var savedOrd = ss.children("#" + itemID).addClass('hidden');
+        // } else {
+          var savedOrd = ss.children("#" + itemID);
+        // }
+        
+
+        // remove all the items
+        child.remove();
+
+        // add the items in turn according to saved order
+        // we need to filter here since the "ui-sortable"
+        // class is applied to all ul elements and we
+        // only want the very first! You can modify this
+        // to support multiple lists - not tested!
+        
+        ss.filter(":first").append(savedOrd);
+      }
+    }
+  },
+  
+	tutorialDrag: function() {
     var t = $("#tutorial_modal");
     t.draggable({ handle: 'thead' });
-  }
+	}
+
 };
 
 
